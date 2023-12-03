0b17a9d1638d69836526b29e59364cbab04e311a
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2023-12-03 12:46:38 +0100

    Etape intermediaire

--- src/main/java/com/adventofcode/challenge/Main.java
+++ src/main/java/com/adventofcode/challenge/Main.java
@@ -7,13 +7,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String nom_fichier = "";
+        String nom_fichier = "src/main/resources/input-test.txt";
         try (Scanner scanner = new Scanner(new File(nom_fichier))) {
 
+            int somme = 0;
+            int lignes = 0, colonnes = 0;
+            int x = 0, y = 0;
             while (scanner.hasNext()) {
-                String line = scanner.nextLine();
+                String line = scanner.nextLine();   // lignes = 140 && colonnes = 140
+                if (colonnes == 0) {
+                    colonnes = line.length();
+                }
+                lignes++;
             }
-            System.out.println("Hello Advent of code !");
+
+            Scanner newScanner = new Scanner(new File(nom_fichier));
+            String[][] engineplan = new String[lignes][colonnes];
+            while (newScanner.hasNext()) {
+                String line = newScanner.nextLine();
+                for(int i = 0; i < line.length(); i++) {
+                    engineplan[x][y] = String.valueOf(line.charAt(i));
+                    y++;
+                }
+                y = 0;
+                x++;
+            }
+            for(String[] linetab : engineplan) {
+                for (String signe : linetab) {
+                    System.out.print(signe + "\t");
+                }
+                System.out.println("\n");
+            }
+
+
+
+            System.out.println(somme);
 
         } catch(FileNotFoundException e) {
             System.out.println("Aucun fichier à lire");
