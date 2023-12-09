7b417a62e5a6e29ccf397ced9638ec756da50ff8
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2023-12-09 17:09:08 +0100

    Day 6 Part One

--- src/main/java/com/adventofcode/challenge/Main.java
+++ src/main/java/com/adventofcode/challenge/Main.java
@@ -1,22 +1,53 @@
 package com.adventofcode.challenge;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+/* Day 6 Part One
+Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?
+Input values :
+Time:        40     82     91     66
+Distance:   277   1338   1349   1063
+* */
 
 public class Main {
     public static void main(String[] args) {
 
-        String nom_fichier = "";
-        try (Scanner scanner = new Scanner(new File(nom_fichier))) {
+        List<int[]> fourRaces = new ArrayList<int[]>();
+        // race[0] = raceDuration && race[1] = recordToBeat && race[2] = wayToWin
+//        // Exemple fourni :
+//        int[] race1 = {7, 9, 0};
+//        int[] race2 = {15, 40, 0};
+//        int[] race3 = {30, 200, 0};
+        int[] race1 = {40, 277, 0};
+        int[] race2 = {82, 1338, 0};
+        int[] race3 = {91, 1349, 0};
+        int[] race4 = {66, 1063, 0};
+        fourRaces.add(race1);
+        fourRaces.add(race2);
+        fourRaces.add(race3);
+        fourRaces.add(race4);
 
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
+        int result = 1;
+        for (int[] race : fourRaces) {
+            int remainingTime = race[0];
+            int speed = 0;
+            int wayToWin = 0;
+            for (int i = remainingTime; i > 0; i--) {
+                if (i * speed > race[1]) {
+                    wayToWin++;
+                }
+                speed++;
             }
-            System.out.println("Hello Advent of code !");
-
-        } catch(FileNotFoundException e) {
-            System.out.println("Aucun fichier à lire");
+            race[2] = wayToWin;
         }
+        for (int[] race : fourRaces) {
+            result *= race[2];
+        }
+
+        // => 505494
+        // => That's the right answer! You are one gold star closer to restoring snow operations [Continue to Part Two]
+
+        System.out.println("Result = " + result);
     }
 }
