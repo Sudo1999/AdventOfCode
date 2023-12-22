2d933ba18db1aec496afcd9ea838e709b87fbe99
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2023-12-22 19:21:05 +0100

    Fichier

--- src/main/java/com/adventofcode/challenge/Main.java
+++ src/main/java/com/adventofcode/challenge/Main.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/* Day 9 Part One :
+Analyze your OASIS report and extrapolate the next value for each history. What is the sum of these extrapolated values?
+* */
+
 public class Main {
     public static void main(String[] args) {
 
-        String nom_fichier = "";
+        String nom_fichier = "src/main/resources/input-9-mirage.txt";
         try (Scanner scanner = new Scanner(new File(nom_fichier))) {
 
             while (scanner.hasNext()) {
