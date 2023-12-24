cf2a1a2ca07e4cc79f53c37771737e2de4db5373
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2023-12-24 15:50:48 +0100

    Day 9 Part Two

--- src/main/java/com/adventofcode/challenge/Main.java
+++ src/main/java/com/adventofcode/challenge/Main.java
@@ -14,23 +14,26 @@ Analyze your OASIS report and extrapolate the next value for each history. What 
 public class Main {
     public static void main(String[] args) {
 
-        String nom_fichier = "src/main/resources/input-9-mirage.txt";   // input-9-mirage.txt
+        String nom_fichier = "src/main/resources/input-9-mirage.txt";
         try (Scanner scanner = new Scanner(new File(nom_fichier))) {
 
-            int somme = 0;
+            int somme1 = 0, somme2 = 0;
             List<Integer> dataValues;
             List<Integer> differenceValues;
-            List<Integer> stepValues;
+            List<Integer> stepAfterValues;
+            List<Integer> stepBeforeValues;
             List<Integer> onlyZeroValues;
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
                 String[] valuesTab = line.split(" ");
                 dataValues = Arrays.stream(valuesTab).map(Integer::parseInt).toList();
-                stepValues = new ArrayList<>();
+                stepAfterValues = new ArrayList<>();
+                stepBeforeValues = new ArrayList<>();
 
                 boolean finalStep = false;
                 while (!finalStep) {
-                    stepValues.add(dataValues.get(dataValues.size()-1));
+                    stepAfterValues.add(dataValues.get(dataValues.size()-1));
+                    stepBeforeValues.add(dataValues.get(0));
                     differenceValues = new ArrayList<>();
                     for (int i = 0; i < dataValues.size()-1; i++) {     // dataLine.size() = 21
                         differenceValues.add(i, (dataValues.get(i+1) - dataValues.get(i)));
@@ -41,14 +44,30 @@ public class Main {
                     onlyZeroValues.removeIf(value -> value.equals(0));
                     finalStep = onlyZeroValues.isEmpty();
                 }
-                int expected = 0;
-                for (int i = stepValues.size()-1; i >= 0; i--) {
-                    expected += stepValues.get(i);
+
+                int expectedAfter = 0;
+                for (int i = stepAfterValues.size()-1; i >= 0; i--) {
+                    expectedAfter += stepAfterValues.get(i);
                 }
-                somme += expected;
+                somme1 += expectedAfter;
+
+                int expectedBefore = 0;
+                for (int i = stepBeforeValues.size()-1; i >= 0; i--) {
+                    expectedBefore = stepBeforeValues.get(i) - expectedBefore;
+                }
+                somme2 += expectedBefore;
             }
             System.out.println();
-            System.out.println("Sum of the extrapolated values = " + somme);
+            System.out.println("Sum of the extrapolated values after the line = " + somme1);
+            System.out.println("Sum of the extrapolated values before the line = " + somme2);
+
+            // That's the right answer! You are one gold star closer to restoring snow operations [Continue to Part Two]
+
+            // Part Two : Rather than adding a zero to the end and filling in the next values of each previous sequence, you should instead
+            //  add a zero to the beginning of your sequence of zeroes, then fill in new first values for each previous sequence.
+            // Extrapolating the previous value for each history, what is the sum of these extrapolated values?
+
+            // That's the right answer! You are one gold star closer to restoring snow operations.
 
         } catch(FileNotFoundException e) {
             System.out.println("Aucun fichier à lire");
