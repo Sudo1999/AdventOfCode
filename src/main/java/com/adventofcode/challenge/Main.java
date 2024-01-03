9362ce8de84566b70d75a024b87526e969152413
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2024-01-03 11:21:01 +0100

    Day 12 Part One

--- src/main/java/com/adventofcode/challenge/Main.java
+++ src/main/java/com/adventofcode/challenge/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 /* Day 12 Part One
-* For each row, count all of the different arrangements of operational and broken springs that meet the given criteria.
+* For each row, count all the different arrangements of operational and broken springs that meet the given criteria.
 * What is the sum of those counts?
 * */
 
@@ -24,63 +24,129 @@ public class Main {
         }
     }
 
+    public static int checkRow(String row, List<Integer> springNumbers) {
+        int alternatives = 0;
+        char charZero = row.charAt(0);
+        String alternateRow;
+        System.out.println("Appel de checkRow() avec row = " + row + " et springNumbers = " + springNumbers);
+
+        if (springNumbers.size() == 0) {
+            for (int i = 0; i < row.length(); i++) {
+                if (row.charAt(i) == '#') {
+                    return 0;
+                }
+            }
+            System.out.println("Return 1");
+            return 1;
+        }
+
+        while (row.length() > 1) {
+            // Cas du point
+            if (charZero == '.') {
+                alternateRow = row.substring(1);
+                return checkRow(alternateRow, springNumbers);
+
+            // Cas du point d'interrogation
+            } else if (charZero == '?') {
+                List<Integer> loopSpringNumbers = new ArrayList<>(springNumbers);
+                // Remplacement par '.'
+                alternateRow = row.substring(1);
+                System.out.println("Cas ? => . (alternatives = " + alternatives + ") => on relance un checkRow");
+                alternatives += checkRow(alternateRow, springNumbers);
+
+                // Remplacement par '#'
+                alternateRow = '#' + row.substring(1);
+                System.out.println("Cas ? => # (alternatives = " + alternatives + ") => on relance un checkRow");
+                return alternatives + checkRow(alternateRow, loopSpringNumbers);
+
+            // Cas du dièse
+            } else if (charZero == '#') {       // Le groupe de springs doit être réglé tout entier à l'intérieur de la condition
+                int target = springNumbers.get(0);
+                int contiguousSprings = 1;
+                alternateRow = row;
+                System.out.println("Cas # :");
+                for (int i = 1; i < target; i++) {
+                    char next = alternateRow.charAt(i);
+                    if (next == '.') {          // L'alternative devient invalide
+                        System.out.println("Le groupe de # est trop court => Return 0");
+                        return 0;
+                    }
+                    if (next == '?') {          // La seule alternative valide est que ce char soit un '#'
+                        alternateRow = alternateRow.substring(0, i) + '#' + alternateRow.substring(i + 1);
+                        //System.out.println("Le groupe est dans la boucle for avec un '?' qui devient '#' => alternateRow = " + alternateRow
+                                //+ " et contiguousSprings = " + contiguousSprings);
+                        contiguousSprings++;
+                    }
+                    if (next == '#') {          // L'alternative reste valide
+                        //System.out.println("Le groupe est dans la boucle for avec un '#' => contiguousSprings = " + contiguousSprings);
+                        contiguousSprings++;
+                    }
+                }
+                System.out.println("Le groupe est arrivé à la valeur target");
+                if (contiguousSprings == target) {
+                    if (alternateRow.charAt(target) == '.') {     // On a identifié le goupe de springs, on peut passer aux suivants
+                        springNumbers.remove(0);
+                        alternateRow = '.' + alternateRow.substring(target + 1);
+                        //System.out.println("Le groupe de # est atteint (alternatives = " + alternatives + "), on relance un checkRow");
+                        return alternatives + checkRow(alternateRow, springNumbers);
+                    } else if (alternateRow.charAt(target) == '?') {     // La seule alternative valide est que ce char soit un '.'
+                        springNumbers.remove(0);
+                        alternateRow = '.' + alternateRow.substring(target + 1);
+                        //System.out.println("Le groupe de # est suivi d'un '?' qui devient '.' (alternatives = " + alternatives
+                                //+ "), on relance un checkRow");
+                        return alternatives + checkRow(alternateRow, springNumbers);
+                    } else if (alternateRow.charAt(target) == '#') {    // Rien ne se passe, l'alternative est invalide (trop de #)
+                        System.out.println("Le groupe de # est trop long => Return 0");
+                        return 0;
+                    }
+                }
+            }
+        }
+        return alternatives;
+    }
+
     public static void main(String[] args) {
 
-        String nom_fichier = "src/main/resources/input-test.txt";       // input-12-hotsprings
+        String nom_fichier = "src/main/resources/input-12-hotsprings.txt";       // input-12-hotsprings
         try (Scanner scanner = new Scanner(new File(nom_fichier))) {
 
             int somme = 0;
             while (scanner.hasNext()) {
-                int alternatives = 0;
+                int alternatives;
                 String line = scanner.nextLine();
                 String initialRow = line.split(" ")[0];
                 initialRow = initialRow + '.';
 
-                // Les variables brokenSum, unknownSum, brokenGroups, unknownGroups, et nonEmptyGroups présentent des données de la ligne existante
-                int brokenSum = 0;
-                int unknownSum = 0;
-                List<Group> brokenGroups = new ArrayList<>();
-                List<Group> unknownGroups = new ArrayList<>();
-                List<Group> nonEmptyGroups = new ArrayList<>();
-                for (int i = 0; i < initialRow.length(); i++) {
-                    int index = i;
-                    if (initialRow.charAt(i) == '#') {
-                        int items = 1;
-                        while (initialRow.charAt(++i) == '#') {
-                            items++;
-                        }
-                        i--;
-                        System.out.println("brokenGroups => index = " + index + " && items = " + items);
-                        Group group = new Group(index, items);
-                        brokenGroups.add(group);
-                        brokenSum += items;
-                    }
-                    if (initialRow.charAt(i) == '?') {
-                        int items = 1;
-                        while (initialRow.charAt(++i) == '?') {
-                            items++;
-                        }
-                        i--;
-                        System.out.println("unknownGroups => index = " + index + " && items = " + items);
-                        Group group = new Group(index, items);
-                        unknownGroups.add(group);
-                        unknownSum += items;
-                    }
-                }
-
-                for (int i = 0; i < initialRow.length(); i++) {
-                    int index = i;
-                    if (initialRow.charAt(i) != '.') {
-                        int items = 1;
-                        while (initialRow.charAt(++i) != '.') {
-                            items++;
-                        }
-                        i--;
-                        System.out.println("nonEmptyGroup ===> index = " + index + " && items = " + items);
-                        Group group = new Group(i, items);
-                        nonEmptyGroups.add(group);
-                    }
-                }
+                // Les variables brokenSum, unknownSum, brokenGroups, et unknownGroups présentent des données de la ligne existante
+//                int brokenSum = 0;      // Pour les #
+//                int unknownSum = 0;     // Pour les ?
+//                List<Group> brokenGroups = new ArrayList<>();       // groupes de un ou plusieurs # consécutifs
+//                List<Group> unknownGroups = new ArrayList<>();      // groupes de un ou plusieurs ? consécutifs
+//                for (int i = 0; i < initialRow.length(); i++) {
+//                    int index = i;
+//                    if (initialRow.charAt(i) == '#') {
+//                        int items = 1;
+//                        while (initialRow.charAt(++i) == '#') {
+//                            items++;
+//                        }
+//                        i--;
+//                        System.out.println("brokenGroups => index = " + index + " && items = " + items);
+//                        Group group = new Group(index, items);
+//                        brokenGroups.add(group);
+//                        brokenSum += items;
+//                    }
+//                    if (initialRow.charAt(i) == '?') {
+//                        int items = 1;
+//                        while (initialRow.charAt(++i) == '?') {
+//                            items++;
+//                        }
+//                        i--;
+//                        System.out.println("unknownGroups => index = " + index + " && items = " + items);
+//                        Group group = new Group(index, items);
+//                        unknownGroups.add(group);
+//                        unknownSum += items;
+//                    }
+//                }
 
                 // La liste springNumbers recense les groupes théoriques de broken springs qui doivent trouver place dans la ligne
                 List<Integer> springNumbers = Arrays.stream((line.split(" ")[1]).split(","))
@@ -97,67 +163,34 @@ public class Main {
                     }
                 }
 
-                System.out.println("Nombre de broken points = " + brokenSum);
-                System.out.println("Nombre de unknown points = " + unknownSum);
-                System.out.println("Assigned points = " + assignedPoints + " including " + (assignedPoints - spaces) + " broken and "
-                        + spaces + " spaces");
-                System.out.println("Non assigned points = " + (initialRow.length() - 1 - assignedPoints) + " sur " + (initialRow.length() - 1));
+//                System.out.println("Nombre de broken points = " + brokenSum);
+//                System.out.println("Nombre de unknown points = " + unknownSum);
+                System.out.print("Assigned points = " + assignedPoints + " including " + (assignedPoints-spaces) + " broken and " + spaces + " spaces. ");
+                System.out.println("Non assigned points = " + (initialRow.length()-1-assignedPoints) + " out of a total of " + (initialRow.length()-1));
                 // Le -1 sert à sortir du décompte le point neutre rajouté à la fin
-                System.out.println();
 
                 /* Je n'y arriverai pas toute seule, la solution fait probablement appel à un concept mathématique bien particulier.
                  => Reddit => Generally speaking, "Dynamic Programming" is an approach to writing algorithms when you can solve a subset
                  of the problem and use it as part of the solution for the larger problem.
                  Memoized recursion is one potential implementation of a dynamic programming algorithm.
-                 * En informatique, la mémoïsation est la mise en cache des valeurs de retour d'une fonction selon ses valeurs d'entrée.
-
-                 J'étais sur la voie, mais quand même très loin de trouver toute seule :
-                 Well, you could analyze the string left to right.
-                 - if it starts with a ., discard the . and recursively check again.
-                 - if it starts with a ?, replace the ? with a . and recursively check again, AND replace it with a # and recursively check again.
-                 - if it starts with a #, check if it is long enough for the first group, check if all characters in the first [grouplength]
-                    characters are not '.', and then remove the first [grouplength] chars and the first group number, and recursively check again.
-                 - at some point you will get to the point of having an empty string and more groups to do - that is a zero,
-                    or having an empty string with zero group to do - that is a one.
-                    - there are more rules to check than these few, which are up to you to find, but this is a way to work out the solution.
+                 * Une fonction récursive est une fonction qui s'appelle elle-même au cours de son exécution.
+                 * La mémoïsation est la mise en cache des valeurs de retour d'une fonction selon ses valeurs d'entrée.
                 * */
 
                 if (assignedPoints == initialRow.length()-1) {
                     alternatives = 1;
                 } else {
-                    List<Integer> stillToBeFound = new ArrayList<>(springNumbers);
-                    for (int number : springNumbers) {
-                        for (int i = 0; i < initialRow.length(); i++) {
-                            if (initialRow.charAt(i) != '.') {
-                                if (initialRow.charAt(i) == '#') {
-                                    final int x = i;
-                                    int springLength = brokenGroups.stream().filter(g -> g.index == x).toList().get(0).items;
-                                    if (number == springLength) {
-                                        if (stillToBeFound.size() > 0) {
-                                            stillToBeFound.remove(0);
-                                        }
-                                        i += (springLength - 1);
-                                        continue;
-                                    } else {    // number est forcément supérieur à springLength, et le caractère d'après est forcément un '?'
-                                        i += (springLength - 1);
-                                        while (number > springLength++) {
-                                            i++;
-                                        }
-                                        if (stillToBeFound.size() > 0) {
-                                            stillToBeFound.remove(0);
-                                        }
-                                    }
-                                } else if (initialRow.charAt(i) == '?') {
-
-                                }
-                            }
-                        }
-                    }
-                } // Fin du calcul des alternatives pour la ligne en cours
+                    alternatives = checkRow(initialRow, springNumbers);
+                }
+                // Fin du calcul des alternatives pour la ligne en cours
+                System.out.println(alternatives);
+                System.out.println();
                 somme += alternatives;
             }
 
-            System.out.println(somme);
+            System.out.println("La somme finale est égale à " + somme);
+
+            // That's the right answer! You are one gold star closer to restoring snow operations [Continue to Part Two]
 
         } catch(FileNotFoundException e) {
             System.out.println("Aucun fichier à lire");
